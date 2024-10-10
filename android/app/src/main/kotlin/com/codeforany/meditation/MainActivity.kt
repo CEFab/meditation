5f760eaa48102787f61b860a162cb658768234e6
CEFab <46172236+CEFab@users.noreply.github.com>
2024-10-10 15:40:34 -0400

    Initial commit, login screen

---
+++ android/app/src/main/kotlin/com/codeforany/meditation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codeforany.meditation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/common/color_extension.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TColor {
+  static Color get primary => const Color(0xff8E97FD);
+  static Color get secondary => const Color(0xff3F414E);
+  static Color get tertiary => const Color(0xffEBEAEC);
+  static Color get sleep => const Color(0xff03174C);
+  static Color get sleepText => const Color(0xffE6E7F2);
+
+  static Color get primaryText => const Color(0xff3F414E);
+  static Color get primaryTextW => const Color(0xffF6F1FB);
+  static Color get secondaryText => const Color(0xffA1A4B2);
+  static Color get txtBG => const Color(0xffF2F3F7);
+}
+
+extension AppContext on BuildContext {
+  Size get size => MediaQuery.sizeOf(this);
+  double get width => size.width;
+  double get height =>
+      size.height; // Getter to retrieve the height of the size property.
+
+  Future push(Widget widget) async {
+    // Method to navigate to a new screen with the given widget.
+    return Navigator.push(
+        this, MaterialPageRoute(builder: (context) => widget));
+  }
+
+  void pop() async {
+    // Method to pop the current screen off the navigation stack.
+    return Navigator.pop(this);
+  }
+}
+
+extension HexColor on Color {
+  // Extension method to add functionality to the Color class.
+  static Color formHex(String hexString) {
+    // Converts a hex string to a Color object.
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7)
+      buffer.write('ff'); // Adds 'ff' if the hex string is in short form.
+    buffer
+        .write(hexString.replaceFirst("#", '')); // Removes the '#' if present.
+    return Color(int.parse(buffer.toString(),
+        radix:
+            16)); // Parses the hex string to an integer and creates a Color object.
+  }
+}
